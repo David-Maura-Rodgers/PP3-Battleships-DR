25d44ec4e810560a33ca53902a1af7f459b81652
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-03-07 22:18:11 +0000

    put for loop inside while loop to get all coordinates entered

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ print(' '.join(row4))
 print(' '.join(row5))
 print("\n")
 
-print("Place your ships at four different coordinates on the board")
+print("Place your ships at four different coordinates on the board.")
 print("The 1st number is the COLUMN, and the 2nd number is the ROW: \n")
 print("NOTE: Please only select from numbers 1 to 5\n")
 
@@ -27,10 +27,6 @@ print("NOTE: Please only select from numbers 1 to 5\n")
 # While Loop to count number of ships placed by player
 ship_placement = False
 ship_number = 0
-position_one = []
-position_two = []
-position_three = []
-position_four = []
 
 while not ship_placement:
     coord_one = input("Please select number for COLUMN: ")
@@ -43,7 +39,20 @@ while not ship_placement:
     horizontal = int(coord_one)
     vertical = int(coord_two)
     player_board[vertical - 1][horizontal - 1] = "@"
-     
+
+    # For Loop for player coordinates
+    # Store player coordinates in two Lists
+    player_coord_x = []
+    player_coord_y = []
+
+    for coord in coord_one:
+        player_coord_x += coord
+    print(player_coord_x)
+
+    for coord in coord_two:
+        player_coord_y += coord
+    print(player_coord_y)
+
     if ship_number == 4:
         ship_placement = True
         print(' '.join(row1))
@@ -54,15 +63,3 @@ while not ship_placement:
         print("\n")
         print("All your ships have been placed!")
 
-# For Loop for player coordinates
-# Store player coordinates in two Lists
-player_coord_x = []
-player_coord_y = []
-
-for coord in coord_one:
-    player_coord_x += coord
-print(player_coord_x)
-
-for coord in coord_two:
-    player_coord_y += coord
-print(player_coord_y)
