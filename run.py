0dfb2b8bcc46f20a4ddb4080a49e26d4a0c0623f
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-03-07 19:39:52 +0000

    tidy up indentation and print statements within While Loop

--- run.py
+++ run.py
@@ -17,8 +17,9 @@ print(' '.join(row4))
 print(' '.join(row5))
 print("\n")
 
-print("Please select where you want to place your ships\n")
-print("The 1st number is the COLUMN, and the 2nd number is the ROW: \n")
+print("Please select where you want to place your ships.\n")
+print("Place your ships at four different coordinates on the board\n")
+print("The 1st number is the COLUMN, and the 2nd number is the ROW:")
 print("Please only select from numbers 1 to 5\n")
 
 # Variable to check number of ships placed by user
@@ -27,39 +28,22 @@ ship_number = 0
 
 # Attempt at While Loop
 while not ship_placement:
-  position_one = input("Please select number for COLUMN: \n")
-  position_two = input("Please select number for ROW: \n")
-  ship_number += 1
+    position_one = input("Please select number for COLUMN: \n")
+    position_two = input("Please select number for ROW: \n")
+    ship_number += 1
 
-  # Convert both position inputs from string to integer
-  horizontal = int(position_one)
-  vertical = int(position_two)
+    # Convert both position inputs from string to integer
+    horizontal = int(position_one)
+    vertical = int(position_two)
 
-  # Updates board with the user input(position variables) and places @     there.
-  player_board[vertical - 1][horizontal - 1] = "@"
-  
-  if ship_number == 4:
-    ship_placement = True
-    print(' '.join(row1))
-    print(' '.join(row2))
-    print(' '.join(row3))
-    print(' '.join(row4))
-    print(' '.join(row5))
-    print("All ships have been placed!")
-
-'''
-# Convert both position inputs from string to integer
-horizontal = int(position_one)
-vertical = int(position_two)
-
-# Updates board with the user input(position variables) and places @ there.
-player_board[vertical - 1][horizontal - 1] = "@"
-
-# https://stackoverflow.com/questions/11178061/print-list-without-brackets-in-a-single-row
-print(' '.join(row1))
-print(' '.join(row2))
-print(' '.join(row3))
-print(' '.join(row4))
-print(' '.join(row5))
-'''
-
+    # Updates board with the user input(position variables) and places @ there.
+    player_board[vertical - 1][horizontal - 1] = "@"
+    
+    if ship_number == 4:
+        ship_placement = True
+        print(' '.join(row1))
+        print(' '.join(row2))
+        print(' '.join(row3))
+        print(' '.join(row4))
+        print(' '.join(row5))
+        print("All ships have been placed!")
