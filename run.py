214bcc5f72a963adb4828b41361d920f7836f2b4
David-Maura-Rodgers <rodgersdavid94@gmail.com>
2022-03-08 16:54:02 +0000

    add position_x and position_y list to store coordinates generated in while loop

--- run.py
+++ run.py
@@ -24,20 +24,28 @@ print("Place your ships at four different coordinates on the board.")
 print("The 1st number is the COLUMN, and the 2nd number is the ROW: \n")
 print("NOTE: Please only select from numbers 1 to 5\n")
 
-
 # While Loop to count number of ships placed by player
 ship_placement = False
 ship_number = 0
 
+# https://stackoverflow.com/questions/4435169/how-do-i-append-one-string-to-another-in-python#comment88685663_4435169
+# Lists to hold cordinates for player's x and y coordinates
+position_x = []
+position_y = []
+
 while not ship_placement:
-    coord_one = input("Please select number for COLUMN: ")
-    coord_two = input("Please select number for ROW: ")
+    input_x = int(input("Please select number for COLUMN: "))
+    input_y = int(input("Please select number for ROW: "))
     ship_number += 1
+    x = position_x.append(input_x)
+    y = position_y.append(input_y)
+    print(position_x)
+    print(position_y)
 
     # Convert both position inputs from string to integer
     # Updates board with the user input(position variables) and places @ there.
-    horizontal = int(coord_one)
-    vertical = int(coord_two)
+    horizontal = int(input_x)
+    vertical = int(input_y)
     player_board[vertical - 1][horizontal - 1] = "@"
 
     if ship_number == 4:
@@ -49,4 +57,5 @@ while not ship_placement:
         print(' '.join(row5))
         print("\n")
         print("All your ships have been placed!")
+        print("\n")
 
